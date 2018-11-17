c1a8132f68d937eebbd04535fa23253304832725
JennicaStiehl <jennica.stiehl@gmail.com>
2018-11-17 10:50:28 -0700

    adds week 1 mythical creatures

---
+++ mythical-creatures/lib/dragon.rb
@@ -0,0 +1,27 @@
+class Dragon
+
+  attr_reader :name,
+              :rider,
+              :color,
+              :hungry
+
+  def initialize(name, color, rider)
+    @name = name
+    @color = color
+    @rider = rider
+    @hungry = 0
+  end
+
+  def hungry?
+    if @hungry >= 3
+      false
+    else
+      true
+    end
+  end
+
+  def eat
+    @hungry += 1
+  end
+
+end
